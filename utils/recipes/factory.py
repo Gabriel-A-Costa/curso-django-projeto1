36015b277ccd3aafdd3747fb46795d2a9e6a33d7
Gabriel Alcantara <gabrielallcantaracosta@gmail.com>
2024-08-08 18:23:28 -0300

    Creat View category

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -11,6 +11,7 @@ fake = Faker('pt_BR')
 
 def make_recipe():
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
@@ -18,6 +19,7 @@ def make_recipe():
         'servings': fake.random_number(digits=2, fix_len=True),
         'servings_unit': 'Porção',
         'created_at': fake.date_time(),
+        'preparation_steps': fake.text(3000),
         'author': {
             'first_name': fake.first_name(),
             'last_name': fake.last_name(),
